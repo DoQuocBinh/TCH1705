1490b1bf2447998abaa7a87d83c85cbd4dd614bb
DoQuocBinh <binhdq@fpt.edu.vn>
2020-09-23 14:11:03 +0700

    first commit

---
+++ ShowCase/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using StringLibrary;
+
+namespace ShowCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World! starts with uppper:" + "Hello World!".StartsWithUpper());
+            Console.WriteLine("hello World! starts with uppper:" + "hello World!".StartsWithUpper());
+
+        }
+    }
+}
